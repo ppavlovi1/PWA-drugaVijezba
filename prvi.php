40e5804ee98938218489073a64226a22b30f50ea
ppavlovi1 <102903091+ppavlovi1@users.noreply.github.com>
2022-04-08 13:53:30 +0200

    Add files via upload

---
+++ prvi.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+if(isset($_POST['option']))
+{
+    echo "
+   checkbox je odabran
+    ";
+}
+else{
+    echo "checkbox nije odabran";
+}
+?>
+
+</body>
+</html>
